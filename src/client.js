c4535757f12ee6e454121f33a1ec877b3455a12c
Thrasos06 <thrasos.vito@gmail.com>
2023-01-16 15:30:50 +0200

    Changed useCdn to false

--- src/client.js
+++ src/client.js
@@ -5,7 +5,7 @@ export const client = sanityClient({
   projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
   dataset: "production",
   apiVersion: "1",
-  useCdn: true,
+  useCdn: false,
   token: process.env.REACT_APP_SANITY_TOKEN,
 });
 
